853e8773c39a8fb9372da050aa3abd44e7c5a577
Christopher Hockenbrocht <christopher@leapyear.io>
2019-03-19 20:18:01 -0700

    add centos install

---
+++ setup_centos.sh
@@ -0,0 +1,38 @@
+# yum installs
+yum install zsh
+yum install neovim
+yum install tmux
+yum install reattach-to-user-namespace
+
+# setup zsh
+sudo command -v zsh | sudo tee -a /etc/shells
+chsh -s zsh
+
+# install powerline modified fonts
+sh ./fonts/install.sh
+
+# setup tmux
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux ~/.tmux
+
+# setup prezto
+ln -fs ~/dotfiles/prezto ~/.zprezto
+ln -fs ~/dotfiles/prezto/runcoms/zlogin ~/.zlogin
+ln -fs ~/dotfiles/prezto/runcoms/zlogout ~/.zlogout
+ln -fs ~/dotfiles/prezto/runcoms/zpreztorc ~/.zpreztorc
+ln -fs ~/dotfiles/prezto/runcoms/zprofile ~/.zprofile
+ln -fs ~/dotfiles/prezto/runcoms/zenv ~/.zenv
+ln -fs ~/dotfiles/prezto/runcoms/zshenv ~/.zshenv
+ln -fs ~/dotfiles/zshrc ~/.zshrc
+
+# setup vim configs
+rm -rf ~/.vim
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.nvim
+
+# link for neovim
+mkdir ~/.config/
+mkdir ~/.config/nvim
+ln -s ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/vim ~/.local/share/nvim/site
